11d51a8ed234ce45c4e8b2bc654777c1a6db2fcc
Kim Harold <pinanonangkim000@gmail.com>
2024-06-11 15:16:04 -0700

    Refactor to basics and install daisyUi

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Navbar asdasd
+        <button className="btn btn-accent">Accent</button>
+    </div>
+    <div>
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+</script>
+
+<template>
+    <DefaultLayout>
+        <h1>Login Page</h1>
+    </DefaultLayout>
+</template>
